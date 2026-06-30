24ba9375ef2f140459283b99e0885bb70a66ab1d
tobiasmanquez <tobias.manquez@gm2dev.com>
2026-06-30 11:35:51 -0300

    Progreso de clase 02, quedan cosas pendientes

--- clase02-variables/2/studio.js
+++ clase02-variables/2/studio.js
@@ -1,38 +1,3 @@
-/*Práctica 1 — Tipos y typeof*/
-
-/* Parte A — Predecir antes de ejecutar */
-
-typeof "Rosario" /* creo que devuelve 'string'*/
-typeof 42 /* creo que devuelve 'number' */
-typeof 3.14 /* creo que 'number'*/
-typeof true /* creo que devuelve 'boolean'*/
-typeof false /* creo que devuelve 'boolean'*/
-typeof undefined /* creo que devuelve 'undefined'*/
-typeof null /* creo que devuelve 'null'*/
-
-
-/* Parte B — Declarar y verificar */
-
-const ciudad = "Mendoza";     /* Pienso que es un texto (String) */
-const habitantes = 115000;    /* Pienso que da un número (Number) */
-const esCap = false;          /* Pienso que es un valor  (Boolean) */
-const apodo = null;           /* Pienso que es un valor nulo (null) */
-let fundacion;                /* Pienso que da (Undefined) */
-
-console.log(typeof ciudad);     /* Devuelve: string */
-console.log(typeof habitantes); /* Devuelve: number */
-console.log(typeof esCap);      /* Devuelve: boolean */
-console.log(typeof apodo);      /* Devuelve: object por el bugggggg*/
-console.log(typeof fundacion);  /* Devuelve: undefined */
-
-/* Parte C — Declarar y verificar */
-
-const nada = null;
-console.log(typeof nada); //? 
-
-/* Pienso que dara null */ /* dio object por el bug de JS*/
-
-
 /* Práctica 2 — var vs let y const */
 
 /* Parte A — El bucle roto */
---
+++ clase02-variables/3/studio.js
@@ -0,0 +1,37 @@
+/* Práctica 3 — Template literals
+Dificultad: 🟢 base
+
+Objetivo
+Reescribir concatenaciones de strings usando template literals (plantillas de texto) con backticks y ${}. */
+
+
+const nombre = "Lautaro";
+const edad   = 17;
+const ciudad = "Rosario";
+const equipo = "Newells";
+
+//1
+console.log(`Hola me llamo ${nombre}, y tengo ${edad} `);
+
+//2
+console.log(`soy de ${ciudad}`);
+
+//3
+console.log(`El doble de mi edad es ${edad * 2}, .`)
+
+//4
+console.log(`soy ${nombre}, de ${ciudad}, y soy de ${equipo}`);
+
+//5
+console.log(`Nombre: ${nombre} \n Edad: ${edad} \n Ciudad: ${ciudad} `);
+
+
+
+
+
+
+
+
+
+
+
